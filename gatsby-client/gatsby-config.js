0874c9389c4394ae76276bbd083739c57d41f364
vivz753 <vivz753@gmail.com>
2021-02-08 16:29:58 -0800

    SAVE: twin.macro, emotion, tailwindcss, postcss, config files barebones INSTALLATION

---
+++ gatsby-client/gatsby-config.js
@@ -0,0 +1,30 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+const tailwindConfig = require("./tailwind.config.js");
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.        
+      }
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(tailwindConfig),
+          require(`autoprefixer`),
+          ...(process.env.NODE_ENV === `production`
+            ? [require(`cssnano`)]
+            : []),
+        ],
+      },
+    },
+  ],
+}
---
+++ gatsby-client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/**/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'bel': ['Belgrano', '-apple-system', 'BlinkMacSystemFont'],
+      'goth': ['Gothic', '-apple-system', 'BlinkMacSystemFont']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
