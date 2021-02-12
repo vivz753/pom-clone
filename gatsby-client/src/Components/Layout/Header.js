304a0d333323880388c144a47f0ecbed26ea0e6a
vivz753 <vivz753@gmail.com>
2021-02-11 16:59:57 -0800

    changes to header and layout

--- gatsby-client/src/Components/Layout/Header.js
+++ gatsby-client/src/Components/Layout/Header.js
@@ -1,18 +1,21 @@
 import React from "react"
 import tw from "twin.macro"
 
+const NavBar = tw.nav`flex flex-wrap md:flex-row flex-col items-center md:items-start`
+const Container = tw.div`container mx-auto flex flex-wrap px-14 py-6 flex-col md:flex-row items-center`
+
 const Header = () => {
   
   return (
-    <header className="bg-white">
-      <div className="container mx-auto flex flex-wrap p-5 my-6 flex-col sm:flex-row items-center">
-        <nav className="text-red-500">
-          Pom Logo
-        </nav>
-      </div>
-      <div>
-        {/* Side drawer icon */}
-      </div>
+    <header className="fixed flex w-full shadow-red-md bg-white">
+      <Container>
+        <NavBar className="text-regular text-xl text-gray-500">
+          POM
+        </NavBar>
+        <p className="ml-auto">
+          Side Drawer
+        </p>
+      </Container>
     </header>
   )
 }
--- gatsby-client/src/Components/Layout/index.js
+++ gatsby-client/src/Components/Layout/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import Header from "./Header"
 import tw, { GlobalStyles } from "twin.macro"
 
-const PageContainer = tw.div`bg-gray-200 text-xl w-1/2`;
+const PageContainer = tw.div`bg-purple-200 text-xl w-full mx-auto p-12`;
 
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <GlobalStyles />
       <Header />
-      <PageContainer>
-        <main>
+      {/* <PageContainer> */}
+        <main className="flex-1">
           { children }
         </main>
-      </PageContainer>
+      {/* </PageContainer> */}
     </div>
   )
 }
