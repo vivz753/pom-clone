cad6a49bf65fc316f81d6356f86e32555ff1de8e
vivz753 <vivz753@gmail.com>
2021-02-11 17:02:44 -0800

    remove router

--- gatsby-client/src/pages/index.js
+++ gatsby-client/src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Router } from "@reach/router"
 import Store from "../Components/Store"
-import Splash from "../Components/Splash"
+import Splash from "../Components/Pages/Splash"
 
 export default function Home() {
   return (
     <Store>
-      <Router basepath="/app">
-        <Splash path="/" />
-      </Router>
+      <Splash />
     </Store>
   )
 }
