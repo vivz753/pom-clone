72654af1994deb7e2d9f2e515896f8a764f2363e
vivz753 <vivz753@gmail.com>
2021-02-11 20:37:07 -0800

    modify shadow colors in tailwind.config.js

--- gatsby-client/tailwind.config.js
+++ gatsby-client/tailwind.config.js
@@ -7,13 +7,13 @@ let makeShadow = (name, rgb) => {
   obj[name] = `0 1px 3px 0 rgba(${rgb}, 0.1), 0 1px 2px 0 rgba(${rgb}, 0.06)`;
   obj[
     name + "-md"
-  ] = `0 4px 6px -1px rgba(${rgb}, 0.2), 0 2px 4px -1px rgba(${rgb}, 0.1)`;
+  ] = `0 4px 6px -1px rgba(${rgb}, 0.2), 0 2px 4px -1px rgba(${rgb}, 0.2)`;
   obj[
     name + "-lg"
-  ] = `0 10px 15px -3px rgba(${rgb}, 0.1), 0 4px 6px -2px rgba(${rgb}, 0.05)`;
+  ] = `0 8px 10px -3px rgba(${rgb}, 0.2), 0 3px 5px -2px rgba(${rgb}, 0.2)`;
   obj[
     name + "-xl"
-  ] = `0 20px 25px -5px rgba(${rgb}, 0.1), 0 10px 10px -5px rgba(${rgb}, 0.04)`;
+  ] = `0 20px 25px -5px rgba(${rgb}, 0.2), 0 10px 10px -5px rgba(${rgb}, 0.3)`;
   obj[name + "-2xl"] = `0 25px 50px -12px rgba(${rgb}, 0.25)`;
   obj[name + "-inner"] = `inset 0 2px 4px 0 rgba(${rgb}, 0.06)`;
   return obj;
