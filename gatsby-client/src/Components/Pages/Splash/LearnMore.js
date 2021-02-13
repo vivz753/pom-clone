5e29a5b78eb03ae1c52ec16244725f3f9860a669
vivz753 <vivz753@gmail.com>
2021-02-12 20:55:49 -0800

    add video phone demo to LearnMore

--- gatsby-client/src/Components/Pages/Splash/LearnMore.js
+++ gatsby-client/src/Components/Pages/Splash/LearnMore.js
@@ -7,23 +7,31 @@ const Container = tw.container`w-full flex flex-col`;
 const PinkContainer = tw(Container)`bg-red-400 flex flex-row`;
 
 const Content2 = tw.container`mx-auto py-32 w-1/2 items-center text-center`
+const Content1 = tw.div`w-full py-24 items-center text-center flex flex-row`
 
-const ButtonStyle = tw`text-red-400 text-base py-1 px-8 mt-4 bg-purple-800 hover:text-purple-800 hover:bg-transparent border-4 border-purple-800 rounded-full`
+
+const ButtonStyle = tw`text-red-400 text-base py-1 px-8 mx-auto mt-4 bg-purple-800 hover:text-purple-800 hover:bg-transparent border-4 border-purple-800 rounded-full`
 
 const LearnMore = () => {
   return (
     <PinkContainer>
-    <Content2>
-      <p className="py-8 px-32 text-5xl text-regular text-white">
+    <Content1>
+      <div className="flex flex-col px-32 w-1/2 mx-auto text-center items-center">
+
+      <p className="py-8 text-5xl text-regular text-white">
         WANT TO KNOW HOW IT WORKS?
       </p>
       <Button style={ButtonStyle}>
-          Learn More
-        </Button>
-    </Content2>
-    <Content2>
-      Phone gif
-    </Content2>
+        Learn More
+      </Button>
+      </div>
+      <div className="w-1/2">
+
+      <video className="object-contain mx-auto h-96 border-4 border-purple-800 rounded-2xl" id="video" autoPlay loop muted>
+        <source src="https://uploads-ssl.webflow.com/5f760442c871757baf72e5ee/5f81c759e8bfd0547a8f72e3_Ui animation for we-transcode.mp4" type="video/mp4" />  
+      </video>
+      </div>
+    </Content1>
   </PinkContainer>
   )
 }
